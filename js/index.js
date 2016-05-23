78f8a703b5c3a68ee5cc62aa83dc796480375ec4
jdrub <j.drubin94@gmail.com>
2016-05-23 05:20:22 -0400

    made layout nicer, setup for tablet and desktop, phone still needs work. button needs to change color or something, and also be disabled when unable to load more

--- js/index.js
+++ js/index.js
@@ -24,17 +24,19 @@ function createReviewHtml(title,timestamp,text){
   var datestring = timestamp.substring(0,timestamp.indexOf('T'));
 
   return '\
-    <div class="mdl-cell mdl-cell--3-col"></div>\
-    <div class="review mdl-cell mdl-cell--6-col">\
-      <div class="review-card mdl-card mdl-shadow--3dp">\
-        <div class="mdl-card__title mdl-card--border">\
-          <h4>'+title+'</h4>\
+    <div class="review mdl-grid">\
+      <div class="mdl-cell mdl-cell--2-col"></div>\
+      <div class="mdl-cell mdl-cell--8-col">\
+        <div class="review-card mdl-card mdl-shadow--2dp">\
+          <div class="mdl-card__title mdl-card--border">\
+            <h4>'+title+'</h4>\
+          </div>\
+          <p class="review-date mdl-card__supporting-text">'+datestring+'</p>\
+          <div class="mdl-card__supporting-text">'+text+'</div>\
         </div>\
-        <p class="review-date mdl-card__supporting-text">'+datestring+'</p>\
-        <div class="mdl-card__supporting-text">'+text+'</div>\
       </div>\
+      <div class="mdl-cell mdl-cell--2-col"></div>\
     </div>\
-    <div class="mdl-cell mdl-cell--3-col"></div>\
     ';
 }
 
@@ -60,20 +62,21 @@ $(document).ready(function(){
     getReviews(numReviews,numReviews+10, localStorage.getItem("searchQuery"), getReviewsCallback);
   });
 
-  $('#searchForm').submit( function(){
+  $(document).keyup(function (e) {
+      if ($(".searchBox").is(":focus") && (e.keyCode == 13)) {
+        var numReviews = parseInt(localStorage.getItem("numReviews"));
+        var searchQuery = $('.searchBox').val();
 
-    var numReviews = parseInt(localStorage.getItem("numReviews"));
-    var searchQuery = $('.searchBox').val();
+        if(localStorage.getItem("searchQuery") != searchQuery){
+          localStorage.setItem("searchQuery",searchQuery);
+          localStorage.setItem("numReviews", 0);
+          numReviews = 0;
+          $('.review').remove();
+        }
 
-    if(localStorage.getItem("searchQuery") != searchQuery){
-      localStorage.setItem("searchQuery",searchQuery);
-      localStorage.setItem("numReviews", 0);
-      numReviews = 0;
-      $('.review').remove();
-    }
-
-    getReviews(numReviews,numReviews+maxReviewsToSend, searchQuery, getReviewsCallback);
-    return false;
+        getReviews(numReviews,numReviews+maxReviewsToSend, searchQuery, getReviewsCallback);
+        return false;
+      }
   });
 
 
