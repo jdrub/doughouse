2fcc87b937c13a06f9a25338a0233dbde7b7973c
jdrub <j.drubin94@gmail.com>
2016-06-04 12:31:32 -0400

    added dkim and spf checks (conveniently provided by mailin)

--- app.js
+++ app.js
@@ -129,8 +129,21 @@ mailin.on('message', function (connection, data, content) {
   // grab list of all valid emails
   var ve = require('./validEmails');
 
+  // email authentication
+  if(data.dkim == "failed"){
+    console.log("dkim failed");
+    return;
+  }
+
+  // email validation
+  if(data.spf == "failed"){
+    console.log("spf failed");
+    return;
+  }
+
   // check for invalid header content
   if(!data.from){
+    console.log("invalid content");
     return;
   }
 
